b44cb4e003a6978b072aebb30eb78a453cc16eaa
Nicole Martin <n.martinpdx@gmail.com>
2021-03-01 14:33:34 -0800

    add new endpoints

---
+++ data/todos.js
@@ -0,0 +1,23 @@
+module.exports = [
+  { id: 1,
+    todo: 'wash clothes',
+    completed: false,
+    
+  },
+  { id: 2,
+    todo: 'wash dishes',
+    completed: false,
+    
+  },
+  { id: 3,
+    todo: 'dog walk',
+    completed: false,
+    
+    
+  },
+  { id: 1,
+    todo: 'call mom',
+    completed: false,
+    
+  },
+];
